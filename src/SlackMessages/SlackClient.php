4c6779261dbaa92c83446b43e30a67762fb8bb23
Tom Metcalfe <iamtommetcalfe@gmail.com>
2023-04-03 18:17:03 +0100

    Added the options to send, delete and list scheduled messages

--- src/SlackMessages/SlackClient.php
+++ src/SlackMessages/SlackClient.php
@@ -187,6 +187,72 @@ class SlackClient implements MessageSenderInterface
     }
 
     /**
+     * Schedules a message to be sent to a Slack channel at a specific time.
+     *
+     * @param string $channelId The channel ID to send the message to.
+     * @param string $text The message text.
+     * @param int $postAt The Unix timestamp for when the message should be posted.
+     * @return stdClass A stdClass containing the entire response from the Slack API.
+     */
+    public function scheduleMessage(string $channelId, string $text, int $postAt): stdClass
+    {
+        $request = $this->createRequest('POST', 'chat.scheduleMessage', [
+            'channel' => $channelId,
+            'text' => $text,
+            'post_at' => $postAt,
+        ]);
+
+        try {
+            $response = $this->httpClient->sendRequest($request);
+            return $this->processResponse($response);
+        } catch (GuzzleException | ClientExceptionInterface $e) {
+            $this->handleException($e);
+        }
+    }
+
+    /**
+     * Retrieves a list of scheduled messages for a given Slack channel.
+     *
+     * @param string $channelId The channel ID to fetch scheduled messages from.
+     * @return stdClass A stdClass containing the entire response from the Slack API.
+     */
+    public function listScheduledMessages(string $channelId): stdClass
+    {
+        $request = $this->createRequest('GET', 'chat.scheduledMessages.list', [
+            'channel' => $channelId,
+        ]);
+
+        try {
+            $response = $this->httpClient->sendRequest($request);
+            return $this->processResponse($response);
+        } catch (GuzzleException | ClientExceptionInterface $e) {
+            $this->handleException($e);
+        }
+    }
+
+    /**
+     * Deletes a scheduled message in a Slack channel.
+     *
+     * @param string $channelId The channel ID where the scheduled message is located.
+     * @param string $scheduledMessageId The ID of the scheduled message to delete.
+     * @return stdClass A stdClass containing the entire response from the Slack API.
+     */
+    public function deleteScheduledMessage(string $channelId, string $scheduledMessageId): stdClass
+    {
+        $request = $this->createRequest('POST', 'chat.deleteScheduledMessage', [
+            'channel' => $channelId,
+            'scheduled_message_id' => $scheduledMessageId,
+        ]);
+
+        try {
+            $response = $this->httpClient->sendRequest($request);
+            return $this->processResponse($response);
+        } catch (GuzzleException | ClientExceptionInterface $e) {
+            $this->handleException($e);
+        }
+    }
+
+    /**
      * Create a request object with the common headers and given method, endpoint, and payload.
      *
      * @param string $method The HTTP method for the request (e.g., 'POST', 'GET', etc.).
--- tests/SlackMessages/SlackClientTest.php
+++ tests/SlackMessages/SlackClientTest.php
@@ -193,4 +193,126 @@ class SlackClientTest extends TestCase
         $this->assertInstanceOf(stdClass::class, $response);
         $this->assertTrue($response->ok);
     }
+
+    /**
+     * Test the scheduleMessage method by providing a channel, message text, and postAt timestamp.
+     */
+    public function testScheduleMessage(): void
+    {
+        $channel = 'C12345678';
+        $text = 'Scheduled message text';
+        $postAt = 1672845600;
+
+        $expectedResponse = json_decode('{
+            "ok": true,
+            "scheduled_message_id": "1234567890.123456",
+            "post_at": 1672845600,
+            "channel": "C12345678"
+        }');
+
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $httpClient->expects($this->once())
+            ->method('sendRequest')
+            ->willReturnCallback(function ($request) use ($expectedResponse) {
+                $this->assertSame('POST', $request->getMethod());
+                $this->assertSame('chat.scheduleMessage', $request->getUri()->getPath());
+                $this->assertSame('application/json', $request->getHeaderLine('Content-Type'));
+                $this->assertJsonStringEqualsJsonString(
+                    json_encode([
+                        'channel' => 'C12345678',
+                        'text' => 'Scheduled message text',
+                        'post_at' => 1672845600,
+                    ]),
+                    (string) $request->getBody()
+                );
+                $response = $this->createMock(ResponseInterface::class);
+                $response->method('getBody')->willReturn(json_encode($expectedResponse));
+                return $response;
+            });
+
+        $slackClient = new SlackClient('test-token', $httpClient);
+
+        $result = $slackClient->scheduleMessage($channel, $text, $postAt);
+        $this->assertEquals($expectedResponse, $result);
+    }
+
+    /**
+     * Test the listScheduledMessages method by providing a channel.
+     */
+    public function testListScheduledMessages(): void
+    {
+        $channel = 'C12345678';
+
+        $expectedResponse = json_decode('{
+            "ok": true,
+            "scheduled_messages": [
+                {
+                    "id": "1234567890.123456",
+                    "channel_id": "C12345678",
+                    "post_at": 1672845600,
+                    "date_created": 1669875600,
+                    "text": "Scheduled message text"
+                }
+            ],
+            "response_metadata": {
+                "next_cursor": ""
+            }
+        }');
+
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $httpClient->expects($this->once())
+            ->method('sendRequest')
+            ->willReturnCallback(function ($request) use ($expectedResponse) {
+                $this->assertSame('GET', $request->getMethod());
+                $this->assertSame('chat.scheduledMessages.list', $request->getUri()->getPath());
+                $this->assertSame('application/json', $request->getHeaderLine('Content-Type'));
+                $response = $this->createMock(ResponseInterface::class);
+                $response->method('getBody')->willReturn(json_encode($expectedResponse));
+                return $response;
+            });
+
+        $slackClient = new SlackClient('test-token', $httpClient);
+
+        $result = $slackClient->listScheduledMessages($channel);
+        $this->assertEquals($expectedResponse, $result);
+    }
+
+    /**
+     * Test the deleteScheduledMessage method by providing a channel and scheduledMessageId.
+     */
+    public function testDeleteScheduledMessage(): void
+    {
+        $channel = 'C12345678';
+        $scheduledMessageId = '1234567890.123456';
+
+        $expectedResponse = json_decode('{
+            "ok": true,
+            "scheduled_message_id": "1234567890.123456",
+            "channel": "C12345678"
+        }');
+
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $httpClient->expects($this->once())
+            ->method('sendRequest')
+            ->willReturnCallback(function ($request) use ($expectedResponse) {
+                $this->assertSame('POST', $request->getMethod());
+                $this->assertSame('chat.deleteScheduledMessage', $request->getUri()->getPath());
+                $this->assertSame('application/json', $request->getHeaderLine('Content-Type'));
+                $this->assertJsonStringEqualsJsonString(
+                    json_encode([
+                        'channel' => 'C12345678',
+                        'scheduled_message_id' => '1234567890.123456',
+                    ]),
+                    (string) $request->getBody()
+                );
+                $response = $this->createMock(ResponseInterface::class);
+                $response->method('getBody')->willReturn(json_encode($expectedResponse));
+                return $response;
+            });
+
+        $slackClient = new SlackClient('test-token', $httpClient);
+
+        $result = $slackClient->deleteScheduledMessage($channel, $scheduledMessageId);
+        $this->assertEquals($expectedResponse, $result);
+    }
 }
